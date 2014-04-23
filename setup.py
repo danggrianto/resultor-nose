c59691a24fac6a4a6c5552dc646fdf481cad223e
daniel anggrianto <daniela@aweber.com>
2014-04-23 10:37:25 -0400

    bump version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 setup(
     name='Resultor',
-    version='0.0.2',
+    version='0.0.3',
+    description='Plugin to send test result to resultor',
+    author='Daniel Anggrianto',
+    author_email='daniela@aweber.com',
     py_modules=['resultor'],
     entry_points={
         'nose.plugins.0.10': [
